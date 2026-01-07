1b5f5b411472f63ff314c52959f454756474e1f5
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-01-07 06:59:16 -0500

    Backup

--- server/db/tennant.go
+++ server/db/tennant.go
@@ -1,17 +1,34 @@
 package db
 
-type Tenant struct {
+import (
+	"fmt"
+
+	"github.com/cgalvisleon/et/envar"
+	"github.com/cgalvisleon/et/utility"
+	"github.com/cgalvisleon/josefina/server/msg"
+)
+
+type Tennant struct {
 	Name string         `json:"name"`
 	Path string         `json:"path"`
 	Dbs  map[string]*DB `json:"dbs"`
 }
 
-var tennant *Tenant
+func NewTennant(name string) (*Tennant, error) {
+	if !utility.ValidStr(name, 0, []string{""}) {
+		return nil, fmt.Errorf(msg.MSG_ARG_REQUIRED, "name")
+	}
+
+	return &Tennant{
+		Name: name,
+		Path: fmt.Sprintf("%s/%s", tennant.Path, name),
+		Dbs:  make(map[string]*DB),
+	}, nil
+}
+
+var tennant *Tennant
 
 func init() {
-	tennant = &Tenant{
-		Name: "",
-		Path: "/data",
-		Dbs:  make(map[string]*DB),
-	}
+	name := envar.GetStr("TENNANT_NAME", "josefina")
+	tennant, _ = NewTennant(name)
 }
