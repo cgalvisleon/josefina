79383b584d47476c25333933538c53494b6c94b3
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-01-09 17:36:40 -0500

    Backup

--- pkg/josefina/tennant.go
+++ pkg/josefina/tennant.go
@@ -77,6 +77,7 @@ func (s *Tennant) getDb(name string) (*DB, error) {
 		Version: s.Version,
 		Path:    fmt.Sprintf("%s/%s", s.Path, name),
 		Schemas: make(map[string]*Schema),
+		Models:  make(map[string]*Model),
 	}
 	s.Dbs[name] = result
 
