c2dc64329be1dc05b2f617ea6a9634fbb6150132
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-01-20 14:04:40 -0500

    Backup

--- pkg/rds/tennant.go
+++ pkg/rds/tennant.go
@@ -3,6 +3,7 @@ package rds
 import (
 	"fmt"
 
+	"github.com/cgalvisleon/et/logs"
 	"github.com/cgalvisleon/et/utility"
 	"github.com/cgalvisleon/josefina/pkg/msg"
 )
@@ -72,6 +73,16 @@ func (s *Tennant) loadCore() error {
 	if err := db.save(); err != nil {
 		return err
 	}
+
+	err = CreateSerie("pqr", "37860631", "%08d", 0)
+	if err != nil {
+		return err
+	}
+	result, err := GetSerie("pqr", "37860631")
+	if err != nil {
+		return err
+	}
+	logs.Debug(result.ToString())
 
 	return nil
 }
