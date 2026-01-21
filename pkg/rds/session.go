793400c8f3ceed4a3c98f994ef993843cac36b4c
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-01-20 22:03:17 -0500

    Backup

--- pkg/rds/session.go
+++ pkg/rds/session.go
@@ -1,1 +1,15 @@
 package rds
+
+import (
+	"github.com/cgalvisleon/et/et"
+)
+
+type Session struct {
+	Id       string `json:"id"`
+	Username string `json:"username"`
+	Expiry   int64  `json:"expiry"`
+}
+
+func (s *Session) Insert(model *Model, new et.Json) (et.Json, error) {
+	return model.Insert(nil, new)
+}
