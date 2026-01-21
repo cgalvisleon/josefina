ccde2851d4fdfb80214a08971160253b83cc554f
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-01-21 15:34:08 -0500

    Backup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"github.com/cgalvisleon/et/envar"
 	"github.com/cgalvisleon/et/logs"
 	serv "github.com/cgalvisleon/josefina/internal/services"
 )
 
 func main() {
+	envar.SetIntByArg("port", "PORT", 3300)
+	envar.SetIntByArg("rpc", "RPC_PORT", 4200)
+
 	srv, err := serv.New()
 	if err != nil {
 		logs.Fatal(err)
