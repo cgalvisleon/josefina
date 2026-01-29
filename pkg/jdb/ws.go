f691a3dd40060bf293eb0c921e898c2621c1a259
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-01-29 14:14:18 -0500

    Backup

--- pkg/jdb/ws.go
+++ pkg/jdb/ws.go
@@ -65,3 +65,109 @@ func HttpTopic(w http.ResponseWriter, r *http.Request) {
 	}))
 	handler.ServeHTTP(w, r)
 }
+
+/**
+* HttpQueue create a queue channel
+* @param w http.ResponseWriter
+* @param r *http.Request
+**/
+func HttpQueue(w http.ResponseWriter, r *http.Request) {
+	handler := applyAuthenticate(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := response.GetBody(r)
+		if err != nil {
+			response.HTTPError(w, r, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		channel := body.Str("channel")
+		if !utility.ValidStr(channel, 0, []string{""}) {
+			response.HTTPError(w, r, http.StatusBadRequest, fmt.Errorf(msg.MSG_ARG_REQUIRED, "channel").Error())
+			return
+		}
+
+		node.ws.Queue(channel)
+	}))
+	handler.ServeHTTP(w, r)
+}
+
+/**
+* HttpStack create a stack channel
+* @param w http.ResponseWriter
+* @param r *http.Request
+**/
+func HttpStack(w http.ResponseWriter, r *http.Request) {
+	handler := applyAuthenticate(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := response.GetBody(r)
+		if err != nil {
+			response.HTTPError(w, r, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		channel := body.Str("channel")
+		if !utility.ValidStr(channel, 0, []string{""}) {
+			response.HTTPError(w, r, http.StatusBadRequest, fmt.Errorf(msg.MSG_ARG_REQUIRED, "channel").Error())
+			return
+		}
+
+		node.ws.Stack(channel)
+	}))
+	handler.ServeHTTP(w, r)
+}
+
+/**
+* HttpRemove create a stack channel
+* @param w http.ResponseWriter
+* @param r *http.Request
+**/
+func HttpRemove(w http.ResponseWriter, r *http.Request) {
+	handler := applyAuthenticate(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := response.GetBody(r)
+		if err != nil {
+			response.HTTPError(w, r, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		channel := body.Str("channel")
+		if !utility.ValidStr(channel, 0, []string{""}) {
+			response.HTTPError(w, r, http.StatusBadRequest, fmt.Errorf(msg.MSG_ARG_REQUIRED, "channel").Error())
+			return
+		}
+
+		err = node.ws.Remove(channel)
+		if err != nil {
+			response.HTTPError(w, r, http.StatusBadRequest, err.Error())
+			return
+		}
+	}))
+	handler.ServeHTTP(w, r)
+}
+
+/**
+* HttpSubscribe create a stack channel
+* @param w http.ResponseWriter
+* @param r *http.Request
+**/
+func HttpSubscribe(w http.ResponseWriter, r *http.Request) {
+	handler := applyAuthenticate(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := response.GetBody(r)
+		if err != nil {
+			response.HTTPError(w, r, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		channel := body.Str("channel")
+		if !utility.ValidStr(channel, 0, []string{""}) {
+			response.HTTPError(w, r, http.StatusBadRequest, fmt.Errorf(msg.MSG_ARG_REQUIRED, "channel").Error())
+			return
+		}
+
+		ctx := r.Context()
+		username := ctx.Value("username").(string)
+		err = node.ws.Subscribe(channel, username)
+		if err != nil {
+			response.HTTPError(w, r, http.StatusBadRequest, err.Error())
+			return
+		}
+	}))
+	handler.ServeHTTP(w, r)
+}
