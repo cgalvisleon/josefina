ccbcbf9fb9fb37f2b897b967b66ac660883d9801
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-01-29 14:33:44 -0500

    Backup

--- pkg/jdb/config.go
+++ pkg/jdb/config.go
@@ -1,66 +1,26 @@
 package jdb
 
 import (
-	"encoding/json"
-	"os"
-
-	"github.com/cgalvisleon/et/envar"
+	"github.com/cgalvisleon/et/file"
 )
 
 type Config struct {
 	Nodes []string `json:"nodes"`
 }
 
+/**
+* getConfig: Returns the config
+* @return *Config, error
+**/
 func getConfig() (*Config, error) {
 	filePath := "./config.json"
-
-	// 1) Intentar abrir
-	f, err := os.Open(filePath)
-	if err != nil {
-		// Si no existe, crearlo con valores por defecto
-		if os.IsNotExist(err) {
-			cfg := Config{Nodes: []string{}}
-
-			b, err := json.MarshalIndent(cfg, "", "  ")
-			if err != nil {
-				return nil, err
-			}
-
-			if err := os.WriteFile(filePath, b, 0o644); err != nil {
-				return nil, err
-			}
-
-			return &cfg, nil
-		}
-		return nil, err
-	}
-	defer f.Close()
-
-	// 2) Leer/parsear
 	var result Config
-	if err := json.NewDecoder(f).Decode(&result); err != nil {
+	err := file.Read(filePath, result)
+	if err != nil {
 		return nil, err
 	}
 
 	return &result, nil
-}
-
-/**
-* writeConfig: Writes the config
-* @param config *Config
-* @return error
-**/
-func writeConfig(config *Config) error {
-	path := envar.GetStr("PATH_DATA", "./data")
-	f, err := os.Create(path + "/config.json")
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	enc := json.NewEncoder(f)
-	enc.SetIndent("", "  ")
-	return enc.Encode(config)
 }
 
 /**
