9945f4b98812236d805b7ccc9919bb7bfb0d7291
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-02-01 09:37:47 -0500

    Backup

--- internal/jdb/model.go
+++ internal/jdb/model.go
@@ -147,18 +147,6 @@ func (s *Model) Init() error {
 		}
 	}
 
-	for _, detail := range s.Details {
-		model, err := node.GetModel(detail.To)
-		if err != nil {
-			return err
-		}
-
-		err = model.Init()
-		if err != nil {
-			return err
-		}
-	}
-
 	s.IsInit = true
 	return nil
 }
