a3a3f63911b984c08467ae75c7b01de602d9a777
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-02-02 15:14:02 -0500

    Backup

--- internal/cache/syn.go
+++ internal/cache/syn.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"encoding/gob"
+	"errors"
 	"fmt"
 	"os"
 	"time"
@@ -12,6 +13,11 @@ import (
 	"github.com/cgalvisleon/et/logs"
 	"github.com/cgalvisleon/et/mem"
 )
+
+type Result struct {
+	result *mem.Item
+	err    error
+}
 
 type MemCache struct{}
 
@@ -81,5 +87,74 @@ func (s *MemCache) Set(require et.Json, response *mem.Item) error {
 	}
 
 	response = result
+	return nil
+}
+
+/**
+* delete: Deletes a cache value
+* @params to string, key string
+* @return error
+**/
+func (s *MemCache) delete(to, key string) (*mem.Item, error) {
+	var response *mem.Item
+	err := jrpc.CallRpc(to, "MemCache.Delete", et.Json{
+		"key": key,
+	}, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+/**
+* Delete: Deletes a cache value
+* @param require et.Json, response *bool
+* @return error
+**/
+func (s *MemCache) Delete(require et.Json, response *bool) error {
+	key := require.Str("key")
+	result, err := Delete(key)
+	if err != nil {
+		return err
+	}
+
+	*response = result
+	return nil
+}
+
+/**
+* get: Deletes a cache value
+* @params to string, key string
+* @return error
+**/
+func (s *MemCache) get(to, key string) (*mem.Item, error) {
+	var response *Result
+	err := jrpc.CallRpc(to, "MemCache.Delete", et.Json{
+		"key": key,
+	}, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response.result, response.err
+}
+
+/**
+* Get: Gets a cache value
+* @param require et.Json, response *Result
+* @return error
+**/
+func (s *MemCache) Get(require et.Json, response *Result) error {
+	key := require.Str("key")
+	result, exists := Get(key)
+	if !exists {
+		return errors.New("key not found")
+	}
+
+	*response = Result{
+		result: result,
+		err:    nil,
+	}
 	return nil
 }
