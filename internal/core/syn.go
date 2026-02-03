cb3b696f370d5d2d7c74e28c6f974de63f17d04e
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-02-03 11:14:41 -0500

    Backup

--- internal/core/syn.go
+++ internal/core/syn.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"encoding/gob"
+
 	"github.com/cgalvisleon/et/et"
 	"github.com/cgalvisleon/et/jrpc"
 	"github.com/cgalvisleon/josefina/internal/mod"
@@ -11,11 +13,17 @@ type Core struct {
 	address   string
 }
 
+type DbResult struct {
+	Exists bool
+	Db     *mod.DB
+}
+
 var (
 	syn *Core
 )
 
 func init() {
+	gob.Register(DbResult{})
 	syn = &Core{}
 }
 
@@ -49,5 +57,39 @@ func (s *Core) CreateDb(require et.Json, response *mod.DB) error {
 	}
 
 	response = result
+	return nil
+}
+
+/**
+* getDb: Gets a database
+* @params to, name string, dest *DbResult
+* @return bool, error
+**/
+func (s *Core) getDb(to, name string, dest *mod.DB) (bool, error) {
+	var response *DbResult
+	err := jrpc.CallRpc(to, "Core.GetDb", et.Json{
+		"name": name,
+	}, &response)
+	if err != nil {
+		return false, err
+	}
+
+	dest = response.Db
+	return response.Exists, nil
+}
+
+/**
+* GetDb: Gets a database
+* @param require et.Json, response *mod.DB
+* @return error
+**/
+func (s *Core) GetDb(require et.Json, response *DbResult) error {
+	name := require.Str("name")
+	exists, err := GetDb(name, response.Db)
+	if err != nil {
+		return err
+	}
+
+	response.Exists = exists
 	return nil
 }
