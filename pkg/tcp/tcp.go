c74145cf17ccb9c2da44ba2b71099a80195d5510
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-02-04 16:45:24 -0500

    Backup

--- pkg/tcp/tcp.go
+++ pkg/tcp/tcp.go
@@ -1,1 +1,15 @@
 package tcp
+
+import "github.com/cgalvisleon/et/tcp"
+
+var (
+	srv *tcp.Server
+)
+
+func New(port int) *tcp.Server {
+	if srv == nil {
+		srv = tcp.New(port)
+	}
+
+	return srv
+}
