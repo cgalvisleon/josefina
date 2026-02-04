6b05877a76d75df8ea93a8fac9e9f4a35d7c08b7
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-02-04 16:47:02 -0500

    Backup

--- pkg/tcp/tcp.go
+++ pkg/tcp/tcp.go
@@ -6,10 +6,23 @@ var (
 	srv *tcp.Server
 )
 
+/**
+* New
+* @param port int
+* @return *tcp.Server
+**/
 func New(port int) *tcp.Server {
 	if srv == nil {
 		srv = tcp.New(port)
 	}
 
 	return srv
+}
+
+/**
+* Init
+* @return error
+**/
+func Init() error {
+	return srv.Listen()
 }
