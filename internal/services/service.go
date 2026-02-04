7c90bfe7f8eab68b56344a3724a560f4e0deaab8
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-02-04 13:49:33 -0500

    Backup

--- internal/services/service.go
+++ internal/services/service.go
@@ -1,14 +1,13 @@
 package http
 
 import (
-	"net/http"
-
 	"github.com/cgalvisleon/et/logs"
 	"github.com/cgalvisleon/et/server"
 	"github.com/cgalvisleon/et/tcp"
+	"github.com/cgalvisleon/et/ws"
 	v1 "github.com/cgalvisleon/josefina/internal/services/v1"
 	"github.com/cgalvisleon/josefina/pkg/jdb"
-	ws "github.com/cgalvisleon/josefina/pkg/ws"
+	"github.com/cgalvisleon/josefina/pkg/websocket"
 )
 
 var (
@@ -24,6 +23,7 @@ type Service struct {
 func New() *Service {
 	result := &Service{
 		ettp: server.New(appName),
+		ws:   ws.New(),
 	}
 
 	err := jdb.Load()
@@ -37,7 +37,7 @@ func New() *Service {
 	result.ettp.Mount("/", latest)
 	result.ettp.Mount("/v1", latest)
 
-	wsHandler := v1.Ws()
+	wsHandler := websocket.Init(result.ws)
 	result.ettp.Mount("/ws", wsHandler)
 
 	return result
@@ -49,13 +49,4 @@ func New() *Service {
 **/
 func (s *Service) Start() {
 	s.ettp.Start()
-}
-
-/**
-* Ws
-* @return http.Handler
-**/
-func (s *Service) websocket() http.Handler {
-	result := ws.Init(s.ws)
-	return result
 }
--- pkg/websocket/handdler.go
+++ pkg/websocket/handdler.go
@@ -1,4 +1,4 @@
-package v1
+package websocket
 
 import (
 	"fmt"
