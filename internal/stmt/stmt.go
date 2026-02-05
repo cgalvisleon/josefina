42386bcbf573e3a20b29706983f1c292692b960c
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-02-05 04:21:20 -0500

    Backup

--- internal/stmt/stmt.go
+++ internal/stmt/stmt.go
@@ -61,27 +61,3 @@ type ChangePasswordStmt struct {
 }
 
 func (ChangePasswordStmt) stmt() {}
-
-type CreateDbStmt struct {
-	Name string
-}
-
-func (CreateDbStmt) stmt() {}
-
-type GetDbStmt struct {
-	Name string
-}
-
-func (GetDbStmt) stmt() {}
-
-type DropDbStmt struct {
-	Name string
-}
-
-func (DropDbStmt) stmt() {}
-
-type UseDbStmt struct {
-	Name string
-}
-
-func (UseDbStmt) stmt() {}
--- internal/stmt/stmt_db.go
+++ internal/stmt/stmt_db.go
@@ -1,1 +1,25 @@
 package stmt
+
+type CreateDbStmt struct {
+	Name string
+}
+
+func (CreateDbStmt) stmt() {}
+
+type GetDbStmt struct {
+	Name string
+}
+
+func (GetDbStmt) stmt() {}
+
+type DropDbStmt struct {
+	Name string
+}
+
+func (DropDbStmt) stmt() {}
+
+type UseDbStmt struct {
+	Name string
+}
+
+func (UseDbStmt) stmt() {}
