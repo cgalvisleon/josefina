fb583f1e185af9117eaae2c7772e3df5532963e3
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-02-05 04:21:51 -0500

    Backup

--- internal/stmt/stmt.go
+++ internal/stmt/stmt.go
@@ -32,32 +32,3 @@ func ToJson(v any) (et.Json, error) {
 type Stmt interface {
 	stmt()
 }
-
-type CreateUserStmt struct {
-	Username string
-	Password string
-}
-
-func (CreateUserStmt) stmt() {}
-
-type GetUserStmt struct {
-	Username string
-	Password string
-}
-
-func (GetUserStmt) stmt() {}
-
-type DropUserStmt struct {
-	Username string
-	Password string
-}
-
-func (DropUserStmt) stmt() {}
-
-type ChangePasswordStmt struct {
-	Username    string
-	OldPassword string
-	NewPassword string
-}
-
-func (ChangePasswordStmt) stmt() {}
--- internal/stmt/stmt_user.go
+++ internal/stmt/stmt_user.go
@@ -1,1 +1,30 @@
 package stmt
+
+type CreateUserStmt struct {
+	Username string
+	Password string
+}
+
+func (CreateUserStmt) stmt() {}
+
+type GetUserStmt struct {
+	Username string
+	Password string
+}
+
+func (GetUserStmt) stmt() {}
+
+type DropUserStmt struct {
+	Username string
+	Password string
+}
+
+func (DropUserStmt) stmt() {}
+
+type ChangePasswordStmt struct {
+	Username    string
+	OldPassword string
+	NewPassword string
+}
+
+func (ChangePasswordStmt) stmt() {}
