2525f41b720e0e1c338429f3b2480c1043c1e7c4
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-02-16 17:52:25 -0500

    Backup

--- internal/stmt/parser_json.go
+++ internal/stmt/parser_json.go
@@ -3,11 +3,12 @@ package stmt
 import (
 	"github.com/cgalvisleon/et/et"
 	"github.com/cgalvisleon/josefina/internal/catalog"
+	"github.com/cgalvisleon/josefina/internal/node"
 )
 
 type Jql struct {
 	query   *catalog.Model
-	command *catalog.Wheres
+	command *node.Wheres
 }
 
 func ParseJson(input et.Json) ([]Stmt, error) {
