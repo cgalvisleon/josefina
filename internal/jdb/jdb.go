1b4bcefe8a72b361eb23ec22a7a5fab551a609cb
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-02-17 14:06:18 -0500

    Backup

---
+++ internal/jdb/jdb.go
@@ -0,0 +1,47 @@
+package jdb
+
+import (
+	"sync"
+
+	"github.com/cgalvisleon/et/tcp"
+	"github.com/cgalvisleon/josefina/internal/catalog"
+)
+
+var node *Node
+
+/**
+* Load: Loads the node
+* @param port int
+* @return *Node
+**/
+func Load(port int) *Node {
+	if node != nil {
+		return node
+	}
+
+	config, err := getConfig()
+	if err != nil {
+		return nil
+	}
+
+	node = &Node{
+		Server:    tcp.NewServer(port),
+		app:       appName,
+		version:   version,
+		isStrict:  config.IsStrict,
+		dbs:       make(map[string]*catalog.DB),
+		models:    make(map[string]*catalog.Model),
+		sessions:  make(map[string]*Session),
+		cache:     make(map[string][]byte),
+		muDB:      sync.RWMutex{},
+		muModel:   sync.RWMutex{},
+		muSession: sync.RWMutex{},
+		muCache:   sync.RWMutex{},
+	}
+	node.lead = &Lead{node: node}
+	node.follow = &Follow{node: node}
+	node.Mount(node.lead)
+	node.Mount(node.follow)
+
+	return node
+}
